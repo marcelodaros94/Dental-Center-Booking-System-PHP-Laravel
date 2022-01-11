189508103e940796e61dda84a9aa1f11594ef19d
Marcelo Da Ros <marcelodaros94@gmail.com>
2022-01-11 16:39:17 -0500

    migrations and model

---
+++ database/migrations/2022_01_11_202941_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date'); 
+            $table->unsignedBigInteger('hour_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('hour_id')->references('id')->on('hours');           
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
