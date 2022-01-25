a81c9df6e027ead0e4424d478965f644bf91fb9f
Marcelo Da Ros <marcelodaros94@gmail.com>
2022-01-25 11:35:02 -0500

    using global scopes to separate 'orderBy' code

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
@@ -10,6 +11,11 @@ use App\Models\Hour;
 class Booking extends Model
 {
     use HasFactory;
+
+    public static function boot(){//executed automatically by Laravel
+        parent::boot();        
+        static::addGlobalScope(new LatestScope);
+    }
 
     //one to many inverse
     public function User()
